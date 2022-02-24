76d719517a07bb58a004606c4cba9d25e5558393
Abhinav Kumar <abhinavkumar045@gmail.com>
2022-02-24 15:53:02 +0530

    Removing unnecessary dir (StartSdk)

--- START_frontend/Sources/StartSdk/sdk/src/main/java/com/reading/start/sdk/core/CalibrateType.java
+++
@@ -1,9 +0,0 @@
-package com.reading.start.sdk.core;
-
-public enum CalibrateType {
-    Center,
-    TopLeft,
-    TopRight,
-    BottomLeft,
-    BottomRight
-}
--- START_frontend/Sources/StartSdk/sdk/src/main/java/com/reading/start/sdk/core/DetectType.java
+++
@@ -1,6 +0,0 @@
-package com.reading.start.sdk.core;
-
-public enum DetectType {
-    RealTime,
-    PostProcessing
-}
